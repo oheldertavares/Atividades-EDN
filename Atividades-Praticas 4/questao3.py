ba8bfcccc4c96d882b5f7913bea18157986424ca
Helder Costa dos Santos] <xh3lderx@gmail.com>
2025-08-28 09:49:14 -0300

    adicao de regras da senha

--- Atividades-Praticas 4/questao3.py
+++ Atividades-Praticas 4/questao3.py
@@ -1,4 +1,6 @@
-# 3 - Criar um código que serve para verificar se uma senha digitada pelo usuário atende a critérios básicos de segurança.
+''' 3 - Criar um código que serve para verificar se uma senha digitada pelo usuário atende a critérios básicos de segurança.
+a - deve ter pelo menos 8 caracteres.
+b - deve conter pelo menos um número'''
 
 while True:
     try:
